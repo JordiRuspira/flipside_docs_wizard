3e86c59ddf19533cf82ece86b6a5d01ba9ce80df
JordiRuspira <34830607+JordiRuspira@users.noreply.github.com>
2023-06-22 10:49:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,19 +36,10 @@ def load_indexes():
     storage_context = StorageContext.from_defaults(persist_dir="./storage")
     index = load_index_from_storage(storage_context)
     return index
-
-def configure():
-    load_dotenv()
-
-configure()
-
-#os.getenv('api_key')
-
-# API Key
-#load_dotenv()  # Load values from .env file
-openai.api_key = os.getenv('api_key')
+ 
 #openai.api_key = os.environ["OPENAI_API_KEY"]
 
+openai.api_key = st.secrets["API_KEY"]
 # Load index
 index = load_indexes()
 
